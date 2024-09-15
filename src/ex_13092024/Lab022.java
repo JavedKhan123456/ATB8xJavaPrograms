c2ea32c7317272398841a5d4c9b071c62c9f7735
javedkhan123456 <kjaved9892khan@gmail.com>
2024-09-15 11:05:48 -0700

    Initial commit

--- src/ex_13092024/Lab022.java
+++ src/ex_13092024/Lab022.java
@@ -1,8 +1,8 @@
 package ex_13092024;
 
-public class Lab022 {
+   public class Lab022 {
     public static void min(String [] args)
     {
-        System.out.println("Hi")
+        System.out.println("Hi");
     }
 }
