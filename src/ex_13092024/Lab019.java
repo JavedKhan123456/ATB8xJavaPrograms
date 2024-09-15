ce2946d2e1680741e0ea5f419bd8c98b5fd55abb
javedkhan123456 <kjaved9892khan@gmail.com>
2024-09-15 07:25:53 -0700

    Initial commit

---
+++ src/ex_13092024/Lab019.java
@@ -0,0 +1,11 @@
+package ex_13092024;
+
+import java.net.StandardSocketOptions;
+
+public class Lab019 {
+    public static void main(String[] args) {
+
+        System.out.print("Hello world ");
+        System.out.print("Javed");
+    }
+}
