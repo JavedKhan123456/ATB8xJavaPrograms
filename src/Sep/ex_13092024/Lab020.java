1addf78c9395f4b287650c397dc03e7050d91d9f
javedkhan123456 <kjaved9892khan@gmail.com>
2024-09-21 10:35:28 -0700

    Initial commit

--- src/Sep/ex_13092024/Lab020.java
+++ src/Sep/ex_13092024/Lab020.java
@@ -2,9 +2,28 @@ package Sep.ex_13092024;
 
 public class Lab020 {
     public static void main(String[] args) {
-        int num=3;
-        int a=1;
-        System.out.printf("The value of %d * %d is  %d",num,a,num*a);
+        int num=10;
+         //Program to print table of 10 using printf
+        System.out.printf("The value of %d * %d is %d",num,1,num*1);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,2,num*2);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,3,num*3);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,4,num*4);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,5,num*5);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,6,num*6);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,7,num*7);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,8,num*8);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,9,num*9);
+        System.out.println();
+        System.out.printf("The value of %d * %d is %d",num,10,num*10);
+
 
     }
 }
